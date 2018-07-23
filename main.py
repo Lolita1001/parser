a640ea2919dfcfff162e4717137b68947a4c2ae3
Lolita1001 <ripmorda@gmail.com>
2018-07-23 13:04:35 +0400

    debug

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def main_calendar():
                     logging.error(req)
                     print('yep calendar ')
                     for q in shared_items:
-                        print(q, " : ", shared_items[q])
+                        print(shared_items['dateTimeMeasure'], q, " : ", shared_items[q])
 
 
 
